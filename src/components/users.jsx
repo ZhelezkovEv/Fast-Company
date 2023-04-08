7b9c8944f634893a4598f4d38ee8d08a3c4c25a0
Евгений <105783088+ZhelezkovEv@users.noreply.github.com>
2023-04-08 14:48:37 +0500

    Add files via upload

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -27,50 +27,52 @@ const Users = () => {
           ) : users.length > 0 ? (
             <table></table>
           ) : (
-            <h2> Никто не тусанет</h2>
+            <h2> Никто не тусанет с тобой сегодня</h2>
           )}
         </span>
       </h2>
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col m-2">Имя</th>
-            <th scope="col m-2">Качества</th>
-            <th scope="col">Профессия</th>
-            <th scope="col">Встретился,раз</th>
-            <th scope="col">Оценка</th>
-            <th scope="col"></th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr key={user._id}>
-              <td>{user.name}</td>
-              <td>
-                {user.qualities.map((qualitie) => (
-                  <span
-                    key={qualitie._id}
-                    className={"badge mx-2 bg-" + qualitie.color}
-                  >
-                    {qualitie.name}
-                  </span>
-                ))}
-              </td>
-              <td>{user.profession.name}</td>
-              <td>{user.completedMeetings}</td>
-              <td>{user.rate}</td>
-              <td>
-                <button
-                  className="btn btn-danger"
-                  onClick={() => handleDelete(user._id)}
-                >
-                  delete
-                </button>
-              </td>
+      {users.length > 0 && (
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Имя</th>
+              <th scope="col">Качества</th>
+              <th scope="col">Профессия</th>
+              <th scope="col">Встретился,раз</th>
+              <th scope="col">Оценка</th>
+              <th scope="col"></th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user._id}>
+                <td>{user.name}</td>
+                <td>
+                  {user.qualities.map((qualitie) => (
+                    <span
+                      key={qualitie._id}
+                      className={"badge mx-2 bg-" + qualitie.color}
+                    >
+                      {qualitie.name}
+                    </span>
+                  ))}
+                </td>
+                <td>{user.profession.name}</td>
+                <td>{user.completedMeetings}</td>
+                <td>{user.rate}</td>
+                <td>
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => handleDelete(user._id)}
+                  >
+                    delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </>
   );
 };
